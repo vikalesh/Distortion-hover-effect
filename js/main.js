1e0ac978117ae17d5f1f2843d31604336c83707d
vikalesh gupta <vikalesh54321@gmail.com>
2020-07-06 21:47:32 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+ new hoverEffect({
+    parent: document.querySelector('.imgwrp1'),
+    intensity: 0.3,
+    // intensity1: 0.25,
+    // intensity2: 0.25,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: '  images/ice.jpg',
+    image2: 'images/ice2.jpg',
+    displacementImage: 'images/ramen.jpg'
+});
+
+
+ new hoverEffect({
+    parent: document.querySelector('.imgwrp2'),
+    intensity: 0.3,
+    // intensity1: 0.25,
+    // intensity2: 0.25,
+    angle1: Math.PI / 2,
+    angle2: -Math.PI / 2,
+    image1: 'images/img19.jpg',
+    image2: 'images/img20.jpg',
+    displacementImage: 'images/displacement/14.jpg'
+});
